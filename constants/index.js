b2a2c23f3312f2af9cfbe94484ff74811faa5e1b
amanswami51 <mramanswami@gmail.com>
2025-05-04 18:26:00 +0530

    update the first converstion of starting the interview

--- constants/index.js
+++ constants/index.js
@@ -99,7 +99,7 @@ export const mappings = {
 export const interviewer = {
   name: "Interviewer",
   firstMessage:
-    "Hello! Thank you for taking the time to speak with me today. I'm excited to learn more about you and your experience.",
+    "Hello! Thank you for being here. Let's begin our conversation and explore your experience. Shall we Start?",
   transcriber: {
     provider: "deepgram",
     model: "nova-3",
